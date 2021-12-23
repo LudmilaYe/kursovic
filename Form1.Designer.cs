7efa304ad2c63328c7eddf70ff239c378509e575
Ludmila Yegupova <yegupova.mila@bk.ru>
2021-12-23 12:58:56 +0800

    Создала форму и оформила форму

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,111 @@ namespace kursovic
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.tbRadius = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbSpeed = new System.Windows.Forms.TrackBar();
+            this.tbCountOfParticlesPerTick = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRadius)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbCountOfParticlesPerTick)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDisplay
+            // 
+            this.picDisplay.Location = new System.Drawing.Point(0, 0);
+            this.picDisplay.Name = "picDisplay";
+            this.picDisplay.Size = new System.Drawing.Size(922, 435);
+            this.picDisplay.TabIndex = 0;
+            this.picDisplay.TabStop = false;
+            // 
+            // tbRadius
+            // 
+            this.tbRadius.Location = new System.Drawing.Point(166, 438);
+            this.tbRadius.Name = "tbRadius";
+            this.tbRadius.Size = new System.Drawing.Size(284, 56);
+            this.tbRadius.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F);
+            this.label1.Location = new System.Drawing.Point(27, 456);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Радиус ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 12F);
+            this.label2.Location = new System.Drawing.Point(28, 513);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Скорость ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F);
+            this.label3.Location = new System.Drawing.Point(608, 457);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Кол-во частиц за тик";
+            // 
+            // tbSpeed
+            // 
+            this.tbSpeed.Location = new System.Drawing.Point(166, 500);
+            this.tbSpeed.Name = "tbSpeed";
+            this.tbSpeed.Size = new System.Drawing.Size(284, 56);
+            this.tbSpeed.TabIndex = 5;
+            // 
+            // tbCountOfParticlesPerTick
+            // 
+            this.tbCountOfParticlesPerTick.Location = new System.Drawing.Point(593, 500);
+            this.tbCountOfParticlesPerTick.Name = "tbCountOfParticlesPerTick";
+            this.tbCountOfParticlesPerTick.Size = new System.Drawing.Size(284, 56);
+            this.tbCountOfParticlesPerTick.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(940, 568);
+            this.Controls.Add(this.tbCountOfParticlesPerTick);
+            this.Controls.Add(this.tbSpeed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbRadius);
+            this.Controls.Add(this.picDisplay);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRadius)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbCountOfParticlesPerTick)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.TrackBar tbRadius;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar tbSpeed;
+        private System.Windows.Forms.TrackBar tbCountOfParticlesPerTick;
     }
 }
 
